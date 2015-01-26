32fe63f8b264cedc01df8b007a92eb09c467ce06
lmp47 <lmpick@zoho.com>
2015-01-26 16:36:08 +0000

    Removed unnecessary field

--- dataanalysis/Anomaly.java
+++ dataanalysis/Anomaly.java
@@ -5,7 +5,6 @@ import AbstractClasses.Trade.Trade;
 import java.util.Date;
 
 abstract class Anomaly {
-    AssetClass asset;
     Trade anomaly;
     Date startTime, endTime; //time period for which it is an anomaly
 }
