7467b3a509a687ab44019bc1e00e7f7bee7331ff
Sean Saville <sean.saville@hotmail.co.uk>
2015-01-26 16:29:56 +0000

    Changed the GUI and data analysis modules to use the SearchListener interface

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -1,13 +1,23 @@
 package AbstractClasses.GUI;
 
+import AbstractClasses.CommunicationLayer.SearchListener;
+import AbstractClasses.CommunicationLayer.SearchResult;
+
 abstract class GUI {
-    public GUI(){
-	throw new UnsupportedOperationException("This method has not yet been implemented.");
+	
+	public GUI(){
+		throw new UnsupportedOperationException("This method has not yet been implemented.");
     }
-    SearchWindow search;
-    ErrorWindow error;
-    GraphWindow graph;
-    DataWindow data;
-    AnalysisWindow analysis;
-    SummaryWindow summary;
+	
+	SearchWindow search;
+	ErrorWindow error;
+	GraphWindow graph;
+	DataWindow data;
+	AnalysisWindow analysis;
+	SummaryWindow summary;
+
+	// This is the method responsible for handling an incoming search result
+	// It's called by the communication layer whenever a search result is ready	
+	@Override	
+	abstract SearchResult getSearchResult(SearchResult s);
 }
--- dataanalysis/DataAnalyser.java
+++ dataanalysis/DataAnalyser.java
@@ -1,10 +1,20 @@
 package AbstractClasses.DataAnalysis;
 
+import AbstractClasses.CommunicationLayer.SearchListener;
+import AbstractClasses.CommunicationLayer.SearchResult;
 import AbstractClasses.Trade.Trade;
 import java.util.List;
 
-abstract class DataAnalyser {
-    abstract void sendAnalysis(); //Sends results of analysis to GUI
-    abstract List<Anomaly> identifyAnomalies(List<Trade> trade);
-    abstract List<TrendAlert> identifyTrends(List<Trade> trade);
+abstract class DataAnalyser implements SearchListener {
+    
+	abstract void sendAnalysis(); //Sends results of analysis to GUI
+   
+	abstract List<Anomaly> identifyAnomalies(List<Trade> trade);
+   
+	abstract List<TrendAlert> identifyTrends(List<Trade> trade);
+	
+	// This is the method responsible for handling an incoming search result
+	// It's called by the communication layer whenever a search result is ready	
+	@Override	
+	abstract SearchResult getSearchResult(SearchResult s);
 }
