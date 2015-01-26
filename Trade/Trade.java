240f78d69bedd0a0c4e96987bd860e23e30e4eb8
lmp47 <lmpick@zoho.com>
2015-01-26 16:32:23 +0000

    Added fields to Trade.java

--- Trade/Trade.java
+++ Trade/Trade.java
@@ -15,11 +15,14 @@ public abstract class Trade {
 	Action action;
 	boolean cleared;
 	Collateralization collateralization;
+	boolean endUserException;
 	boolean bespoke;
-	boolean exceptionVenue; // true for ON, false for OFF
+	boolean executionVenue; // true for ON, false for OFF
 	boolean blockTrades;
 	Date effectiveDate;
 	Date endDate;
+	String dayCountConvention // e.g. ACT/360 and 1/1 30/360
+	Currency settlementCurrency;
 	AssetClass assetClass;
 	SubAssetClass subAssetClass;
 	UPI taxonomy; //TODO We need to understand what is the structure of this, inside the .csv files
@@ -27,11 +30,30 @@ public abstract class Trade {
 	String underlyingAsset1;
 	String underlyingAsset2;
 	String priceNotationType;
-	int priceNotation;
+	double priceNotation;
+	String additionalPriceNotationType;
+	double additionalpriceNotation;
 	Currency notionalCurrency1;
 	Currency notionalCurrency2;
-	long roundedNotionalAmount1;
-	long roundedNotionalAmount2;
+	long roundedNotionalAmount1; //probably shouldn't be a long, since some values are e.g. 95,000,000+
+	long roundedNotionalAmount2; //also has +
+	String paymentFrequency1; //e.g. 3M, 1M, 0, but also dates like 2016-01-29
+	String paymentFrequency2; //e.g. 1D, 1T, 1M
+	String resetFrequency1; //e.g. 2024-10-10
+	String resetFrequency2; //e.g. 1D, 1M, 1T
+	String embeddedOption; //e.g. EMBED1
 	
-	// Whole bunch of extra stuff here, not sure how relevant it really is	
+	//options
+	double optionStrikePrice;
+	String optionType;
+	String optionFamily;
+	Currency optionCurrency;
+	double optionPremium; //e.g. 0.00034 and 90250
+	Date optionLockPeriod;
+	Date optionExpirationDate;
+	String priceNotation2Type;
+	double priceNotation2;
+	String priceNotation3Type;
+	double priceNotation3;
+	
 }
